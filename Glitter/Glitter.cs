47e4e72c712b5c815ed91bfccc254054d5e3285f
SnowyTom <tanaka-tomoyuki-wy@ynu.jp>
2020-06-27 20:34:23 +0900

    Reduce ConsoleOutput

--- Glitter/Glitter.cs
+++ Glitter/Glitter.cs
@@ -34,49 +34,44 @@ namespace Glitter
 
         }
 
-        public void Calc()
+        public void Calc(bool ConsoleOut = false)
         {
             var weightedGraphs = new CreateWeightedGraph(graphs.VerticalGraph, graphs.HorizontalGraph, wires);
             var selection = new WeightedGraphSelection(weightedGraphs.weightedDirectedGraph, weightedGraphs.weightedUndirectedGraph, graphs.LocalMaximumDensity, wires, graphs.HorizontalGraph);
             var glitter_result = selection.Selection();
-
-            Console.WriteLine("Returned WCG");
-            selection.WeightedDirectedGraph.Edges.ToString<Edge>(format: "{0}\n", end: "", begin: "").Write();
-
-            Console.WriteLine("Routing Order is");
             var routingOrder = selection.WeightedDirectedGraph.TopologicalSort().Where(a => a != "Top" && a != "Bot");
-            routingOrder.ToString<string>().Write();
-            HeightOfHorizontalWire = new List<(string, double)>();
-            Console.WriteLine("Routing Height(from top boundary) is");
-            HeightOfHorizontalWire = routingOrder.Select(a => (a, CreateChainWeight.Ancestor(selection.WeightedDirectedGraph)[a])).ToList();
-            HeightOfHorizontalWire.Select(a => a.Item2).ToString<double>().Write();
-            Console.WriteLine("Channel Height");
+            HeightOfHorizontalWire =new List<(string, double)>(routingOrder.Select(a => (a, CreateChainWeight.Ancestor(selection.WeightedDirectedGraph)[a])).ToList());
             var channelHeight = CreateChainWeight.Ancestor(selection.WeightedDirectedGraph)["Bot"];
             this.channelHeight = channelHeight;
-            Console.WriteLine(channelHeight);
-
-
             calc = new CalcLength(upper, lower, HeightOfHorizontalWire, channelHeight, wires);
-            foreach (var (a, b) in HeightOfHorizontalWire)
-            {
-                Console.Write(calc.GetLength(a));
-                Console.WriteLine(calc.GetInductance(a));
-            }
-#if DEBUG
-            Console.WriteLine("VCG");
-            graphs.VerticalGraph.Edges.ToString<Edge>(format: "{0}\n", end: "", begin: "").Write();
-            Console.WriteLine("HCG");
-            graphs.HorizontalGraph.Edges.ToString<Edge>(format: "{0}\n", end: "", begin: "").Write();
 
-            Console.WriteLine("Init WCG(Directed)");
-            weightedGraphs.weightedDirectedGraph.Edges.ToString<Edge>(format: "{0}\n", end: "", begin: "").Write();
-            Console.WriteLine("Init WCG(unDirected)");
-            weightedGraphs.weightedUndirectedGraph.Edges.ToString<Edge>(format: "{0}\n", end: "", begin: "").Write();
-#endif
+            if (ConsoleOut == true)
+            {
+                Console.WriteLine("Returned WCG");
+                selection.WeightedDirectedGraph.Edges.ToString<Edge>(format: "{0}\n", end: "", begin: "").Write();
+                Console.WriteLine("Routing Order is");
+                routingOrder.ToString<string>().Write();
+                Console.WriteLine("Routing Height(from top boundary) is");
+                HeightOfHorizontalWire.Select(a => a.Item2).ToString<double>().Write();
+                Console.WriteLine("Channel Height");
+                Console.WriteLine(channelHeight);
+                foreach (var (a, b) in HeightOfHorizontalWire)
+                {
+                    Console.Write(calc.GetLength(a));
+                    Console.WriteLine(calc.GetInductance(a));
+                }
+                Console.WriteLine("VCG");
+                graphs.VerticalGraph.Edges.ToString<Edge>(format: "{0}\n", end: "", begin: "").Write();
+                Console.WriteLine("HCG");
+                graphs.HorizontalGraph.Edges.ToString<Edge>(format: "{0}\n", end: "", begin: "").Write();
+                Console.WriteLine("Init WCG(Directed)");
+                weightedGraphs.weightedDirectedGraph.Edges.ToString<Edge>(format: "{0}\n", end: "", begin: "").Write();
+                Console.WriteLine("Init WCG(unDirected)");
+                weightedGraphs.weightedUndirectedGraph.Edges.ToString<Edge>(format: "{0}\n", end: "", begin: "").Write();
+            }
         }
 
-        public void
-        WriteGlitterCSV()
+        public void WriteGlitterCSV()
         {
             using (var streamWriter = new StreamWriter("glitterResult.csv"))
             {
